fad5ddce308d4a3bc0055ab1e761fe36415257dd
Lisa Olson <olson.lisa94@gmail.com>
2018-04-27 16:32:11 +0000

    updates error message to print file name

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	fd = fopen(argv[1], "r");
 	if (fd == NULL)
 	{
-		printf("Error: Can't open file <file>\n");
+		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	oc = malloc(sizeof(char) * 10);
