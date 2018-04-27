26f0ab18b55434b31147a7603943b37404a0a9c2
Lisa Olson <olson.lisa94@gmail.com>
2018-04-27 04:53:33 +0000

    updates betty compliance

--- div.c
+++ div.c
@@ -1,39 +1,38 @@
 #include "monty.h"
 
 /**
- * div - divides second top element by top element
+ * divide - divides second top element by top element
  * @stack: doubly linked list stack
  * @line_number: number of lines
  * Return: void
 */
 void divide(stack_t **stack, unsigned int line_number)
 {
-        int total;
-        int stack_count;
-        stack_t *temp;
+	int total;
+	int stack_count;
+	stack_t *temp;
 
-        stack_count = 0;
-        temp = *stack;
-        while (temp != NULL)
-        {
-                temp = temp->next;
-                stack_count++;
-        }
-        if (stack_count < 2)
-        {
-                printf("L%u: can't div, stack too short\n", line_number);
-                exit(EXIT_FAILURE);
-        }
-
+	stack_count = 0;
+	temp = *stack;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		stack_count++;
+	}
+	if (stack_count < 2)
+	{
+		printf("L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	if ((*stack)->n == 0)
 	{
 		printf("L%u: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-        total = ((*stack)->next)->n / (*stack)->n;
-        ((*stack)->next)->n = total;
-        temp = *stack;
-        *stack = (*stack)->next;
-        (*stack)->prev = NULL;
-        free(temp);
+	total = ((*stack)->next)->n / (*stack)->n;
+	((*stack)->next)->n = total;
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
 }
